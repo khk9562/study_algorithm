24fc71f075b1a4a1f9e646ae2df078aa3a62a165
stella <khk9562@naver.com>
2024-05-14 16:24:32 +0900

    문자열 관련 문제풀이들

---
+++ 240514/문자열.py
@@ -0,0 +1,4 @@
+T = int(input())
+for i in range(T):
+    x = input()
+    print(x[0]+x[-1])
---
+++ 240514/숫자의 합.py
@@ -0,0 +1,6 @@
+N = int(input())
+X = input()
+result =[]
+for x in X:
+    result.append(int(x))
+print(sum(result))
